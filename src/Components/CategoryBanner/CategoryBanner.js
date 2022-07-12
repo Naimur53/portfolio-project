fe240abaff959ad37f7e2ffe9e41ffe30c9580a1
Naimur53 <naimurrhman53@gmail.com>
2022-07-12 15:18:28 +0600

    'doe'

--- src/Components/CategoryBanner/CategoryBanner.js
+++ src/Components/CategoryBanner/CategoryBanner.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { motion } from 'framer-motion'
 const CategoryBanner = () => {
     return (
-        <Box className='  bg-center py-5 mt-5 md:mt-0'>
+        <Box className='  bg-center py-5 mt-10 md:mt-0'>
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} style={{ background: '#0000009a' }} className=' 	'>
                 <div className=' text-center   '>
                     <Box sx={{ fontSize: { md: "200px", xs: '70px' } }} className='leading-none letter  font-bold'>GALLERY</Box>
