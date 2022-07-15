1ab2d3221194b4c149c05414ac6b9f0c407e1880
Naimur53 <naimurrhman53@gmail.com>
2022-07-15 16:04:00 +0600

    'done'

--- src/Components/HomeCategory/HomeCategoryImg.js
+++ src/Components/HomeCategory/HomeCategoryImg.js
@@ -12,23 +12,23 @@ const HomeCategoryImg = ({ mp, title, thumbnail, _id }) => {
             <div className='overflow-hidden block text-gray-50 transition duration-300 brightness-50 hover:no-underline no-underline hover:brightness-100 grayscale hover:grayscale-0  pb-20 w-full relative cursor-pointer'>
 
                 <div className='overflow-hidden w-full h-full'>
-                    <motion.div
-                        initial={{
-                            x: 0,
-                            y: 0,
-                            scale: 1.5,
-                        }}
-                        animate={{
-                            x: mp.x / 10,
-                            y: mp.y / 10,
-                            scale: 1.5,
+                    <div
+                    // initial={{
+                    //     x: 0,
+                    //     y: 0,
+                    //     scale: 1.5,
+                    // }}
+                    // animate={{
+                    //     x: mp.x / 10,
+                    //     y: mp.y / 10,
+                    //     scale: 1.5,
 
-                            transition: { ease: "easeOut", duration: 3 }
-                        }}
+                    //     transition: { ease: "easeOut", duration: 3 }
+                    // }}
                     >
                         <Image priority width={200} height={200} layout={'raw'} src={thumbnail} className=' block w-full ' alt='Image' ></Image>
 
-                    </motion.div>
+                    </div>
                 </div>
                 <div className=" pointer-events-none 	w-full">
                     <h1 className="uppercase">{title.slice(0, 40)}</h1>
