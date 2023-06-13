de6b82e6c82b3efac5b0c096de114af56ba6451b
naimur53 <naimurrhman53@gmail.com>
2023-06-13 22:02:55 +0600

    doen

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,84 @@
 import HomeBanner from "../src/Components/HomeBanner/HomeBanner";
-import HomeCategory from "../src/Components/HomeCategory/HomeCategory"
-import { addCollection, addHomeBlog, addHomeCategory, addScrollValue } from '../src/dataSlice/dataSlice'
+import HomeCategory from "../src/Components/HomeCategory/HomeCategory";
+import {
+  addCollection,
+  addHomeBlog,
+  addHomeCategory,
+  addScrollValue,
+} from "../src/dataSlice/dataSlice";
 import { wrapper } from "../src/store/store";
 import { motion } from "framer-motion";
 import { Container, Grid, Typography, Button } from "@mui/material";
-import { useDispatch } from 'react-redux';
-import { useEffect, useRef } from 'react';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { useDispatch } from "react-redux";
+import { useEffect, useRef } from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Image from "next/image";
 import AboutMe from "../src/Components/AboutMe/AboutMe";
 import HomeBlog from "../src/Components/HomeBlog/HomeBlog";
 import HomeTextSection from "../src/Components/HomeTextSection/HomeTextSection";
-import Head from '../src/Components/Head/Head'
+import Head from "../src/Components/Head/Head";
 import Footer from "../src/Components/AboutPages/Footer";
 import ParallaxLayerBackground from "../src/Components/ParallaxLayerBackground/ParallaxLayerBackground";
 import ContactSections from "../src/Components/ContactSections/ContactSections";
 
 export default function Home() {
-  const dispatch = useDispatch()
-  const parallaxRef = useRef()
+  const dispatch = useDispatch();
+  const parallaxRef = useRef();
 
   const innerRoute = (value) => {
     parallaxRef.current.scrollTo(value);
-  }
+  };
   useEffect(() => {
-    if (!parallaxRef.current || !parallaxRef.current.container) return
+    if (!parallaxRef.current || !parallaxRef.current.container) return;
     parallaxRef.current.container.current.onscroll = () => {
-      dispatch(addScrollValue(parallaxRef.current.current / parallaxRef.current.space))
-    }
+      dispatch(
+        addScrollValue(parallaxRef.current.current / parallaxRef.current.space)
+      );
+    };
     return () => {
-      return dispatch(addScrollValue(0))
-    }
-  })
+      return dispatch(addScrollValue(0));
+    };
+  });
   return (
     <>
-      <Head
-        title="John Baggen"
-      ></Head>
+      <Head title="John Baggen"></Head>
       <Parallax ref={parallaxRef} pages={6}>
-        <ParallaxLayer
-          offset={0}
-          speed={.1}  >
+        <ParallaxLayer offset={0} speed={0.1}>
           <HomeBanner innerRoute={innerRoute}></HomeBanner>
         </ParallaxLayer>
         <ParallaxLayer
           offset={0}
-          speed={.1}
-          style={{ zIndex: '-1' }}
-        >
-
-        </ParallaxLayer>
+          speed={0.1}
+          style={{ zIndex: "-1" }}
+        ></ParallaxLayer>
         <ParallaxLayerBackground
           offset={0}
-          speed={.1}
-          url='https://i.ibb.co/n7xmh1M/NEW-Background.jpg'
-          style={{ zIndex: '-1' }}
+          noBackground={true}
+          speed={0.1}
+          url="https://i.ibb.co/n7xmh1M/NEW-Background.jpg"
+          style={{ zIndex: "-1" }}
         />
-        <ParallaxLayer
-          offset={1}
-          speed={.1}>
+        <ParallaxLayer offset={1} speed={0.1}>
           <HomeTextSection innerRoute={innerRoute}></HomeTextSection>
-
         </ParallaxLayer>
         <ParallaxLayerBackground
           offset={1}
-          speed={.1}
-          url='https://i.ibb.co/n7xmh1M/NEW-Background.jpg'
-          style={{ zIndex: '-1' }}
+          speed={0.1}
+          url="https://i.ibb.co/n7xmh1M/NEW-Background.jpg"
+          style={{ zIndex: "-1" }}
         />
-        <ParallaxLayer
-          offset={2}
-          speed={.5}
-          sticky={{ start: 2, end: 3 }}
-        >
+        <ParallaxLayer offset={2} speed={0.5} sticky={{ start: 2, end: 3 }}>
           <AboutMe innerRoute={innerRoute}></AboutMe>
         </ParallaxLayer>
         <ParallaxLayerBackground
           offset={2}
-          speed={.5}
-          style={{ zIndex: '-1' }}
+          speed={0.5}
+          style={{ zIndex: "-1" }}
         />
         <ParallaxLayerBackground
           offset={3}
-          speed={.5}
-          style={{ zIndex: '-1' }}
+          speed={0.5}
+          style={{ zIndex: "-1" }}
         />
         {/* <ParallaxLayerBackground
           offset={4}
@@ -116,8 +111,8 @@ export default function Home() {
         </ParallaxLayer> */}
         <ParallaxLayerBackground
           offset={4}
-          speed={.5}
-          style={{ zIndex: '-1' }}
+          speed={0.5}
+          style={{ zIndex: "-1" }}
         />
         {/* <ParallaxLayer
           offset={9}
@@ -133,25 +128,23 @@ export default function Home() {
         </ParallaxLayer> */}
         <ParallaxLayer
           offset={4}
-          speed={.3}
+          speed={0.3}
           sticky={{ start: 4, end: 5 }}
           style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-
         >
           <ContactSections></ContactSections>
         </ParallaxLayer>
 
         <ParallaxLayerBackground
           offset={5}
-          speed={.3}
-          style={{ zIndex: '-1' }}
+          speed={0.3}
+          style={{ zIndex: "-1" }}
         />
-
-      </Parallax >
+      </Parallax>
     </>
-  )
+  );
 }
--- src/Components/ParallaxLayerBackground/ParallaxLayerBackground.js
+++ src/Components/ParallaxLayerBackground/ParallaxLayerBackground.js
@@ -1,68 +1,88 @@
-import { ParallaxLayer } from '@react-spring/parallax';
-import React, { useRef } from 'react';
-import { motion, } from 'framer-motion'
-import { useInView } from 'react-intersection-observer';
-import Image from 'next/image';
-const ParallaxLayerBackground = ({ className, factor, offset, url, speed, style }) => {
+import { ParallaxLayer } from "@react-spring/parallax";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import Image from "next/image";
+const ParallaxLayerBackground = ({
+  noBackground,
+  className,
+  factor,
+  offset,
+  url,
+  speed,
+  style,
+}) => {
+  const [ref, inView] = useInView();
 
-    const [ref, inView] = useInView()
+  return (
+    <ParallaxLayer
+      offset={offset}
+      speed={speed}
+      style={style}
+      factor={factor ? factor : 1}
+    >
+      <div
+        style={{
+          backgroundImage: noBackground
+            ? ""
+            : url
+            ? `url(${url})`
+            : "url(https://i.ibb.co/n7xmh1M/NEW-Background.jpg)",
+        }}
+        className={
+          className
+            ? "banner-wrap bg-cover h-full w-full pointer-events-none " +
+              className
+            : "banner-wrap bg-cover h-full w-full pointer-events-none"
+        }
+      >
+        <div ref={ref} className="w-full flex h-full items-end   ">
+          {
+            <motion.div
+              className="relative w-full"
 
-    return (
-        <ParallaxLayer
-            offset={offset}
-            speed={speed}
-            style={style}
-            factor={factor ? factor : 1}
-        >
-            <div
-                style={{
-                    backgroundImage: url ? `url(${url})` : 'url(https://i.ibb.co/n7xmh1M/NEW-Background.jpg)'
+              // initial={{
+              //     width: '0%'
+              // }}
+              // animate={inView ? {
+              //     width: '100%',
+              //     transition: { type: 'ease', duration: 1 }
+              // } : {
+              //     width: '0%'
+              // }}
+            >
+              <img
+                className="w-full mt-15  "
+                src="https://i.ibb.co/R0ngLrJ/Adobe-Stock-477001574-kopi-ren.png"
+                width={700}
+                height={100}
+                alt="background web"
+                priority
+                layout="raw"
+              ></img>
 
+              <motion.div
+                className="bg-black absolute inset-0 shadow-overlay"
+                initial={{
+                  x: " 0%",
                 }}
-                className={className ? "banner-wrap bg-cover h-full w-full pointer-events-none " + className : "banner-wrap bg-cover h-full w-full pointer-events-none"}>
-                <div ref={ref} className='w-full flex h-full items-end   '>
-                    {
-                        <motion.div
-
-                            className="relative w-full"
-
-                        // initial={{
-                        //     width: '0%'
-                        // }}
-                        // animate={inView ? {
-                        //     width: '100%',
-                        //     transition: { type: 'ease', duration: 1 }
-                        // } : {
-                        //     width: '0%'
-                        // }}
-
-                        >
-
-                            <img className='w-full mt-15  ' src='https://i.ibb.co/R0ngLrJ/Adobe-Stock-477001574-kopi-ren.png' width={700} height={100} alt='background web' priority layout='raw'></img>
-
-                            <motion.div
-                                className='bg-black absolute inset-0 shadow-overlay'
-
-                                initial={{
-                                    x: ' 0%'
-                                }}
-                                animate={inView ? {
-                                    x: '100%',
-                                    transition: { type: 'ease', duration: 1 }
-                                } : {
-                                    x: ' 0%'
-                                }}
-                            >
-
-                            </motion.div>
-                        </motion.div>
-                    }
-
-
-                </div>
-            </div>
-        </ParallaxLayer >
-    );
+                animate={
+                  inView
+                    ? {
+                        x: "100%",
+                        transition: { type: "ease", duration: 1 },
+                      }
+                    : {
+                        x: " 0%",
+                      }
+                }
+              ></motion.div>
+            </motion.div>
+          }
+        </div>
+      </div>
+    </ParallaxLayer>
+  );
 };
 
 export default ParallaxLayerBackground;
